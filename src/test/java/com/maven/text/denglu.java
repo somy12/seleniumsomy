bf9bca414c866b593257b4569d447638e976ab3f
1945284117@qq.com <1945284117@qq.com>
2021-07-31 15:18:51 +0800

    代码更改

--- src/test/java/com/maven/text/denglu.java
+++ src/test/java/com/maven/text/denglu.java
@@ -9,7 +9,9 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.sql.*;
-
+import java.util.ArrayList;
+import java.util.List;
+//用ui自动化进行兼容测试
 public class denglu {
     //static WebDriver webdriver = null;//当前类里面都能用到这个对象
 
@@ -17,9 +19,6 @@ public class denglu {
         WebDriver webdriver = null;
         try {
             System.setProperty("webdriver.firefox.bin", "C:\\Program Files\\Mozilla Firefox\\firefox.exe");//导入驱动
-            webdriver = new FirefoxDriver();
-
-            System.setProperty("webdriver.firefox.bin", "C:\\Python27\\geckodriver.exe");//导入驱动
             webdriver = new FirefoxDriver();//启用谷歌浏览器
             webdriver.get("http://localhost:8080/DagShop06/");//打开网址
             String title = webdriver.getTitle();//检测
@@ -52,7 +51,7 @@ public class denglu {
 
 
 
-    public void cha01() throws Exception {//从数据库里面获取用户密码
+    public void cha01(String abc) throws Exception {//从数据库里面获取用户密码
         String driverClassName = "com.mysql.jdbc.Driver";
         String url = "jdbc:mysql://localhost:3306/store";
         String username = "root";
@@ -67,8 +66,9 @@ public class denglu {
             String cname = rs.getString("cname");
             String password1 = rs.getString("password1");
             denglu dl = new denglu();
-            dl.dl01(cname,password1);
-
+//            dl.dl01(cname,password1);
+            jrx xx = new jrx();
+            xx.dl01(cname,password1,abc);
             System.out.println(cname+" "+password1);
         }
         rs.close();
@@ -78,9 +78,17 @@ public class denglu {
     }
     @Test
     public void dl02()  {
+        List<String> list = new ArrayList<String>();
+        list.add("chrome");
+        list.add("firefox");
+
         denglu dl = new denglu();
         try {//扑获异常，防止程序崩溃
-            dl.cha01();
+            for (int i=0;i< list.size();i++){
+                String browser = list.get(i);
+                dl.cha01(browser);
+            }
+
         } catch (Exception e) {
             System.out.println("当前登录错误");
         }
